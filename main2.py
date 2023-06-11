9975768e659a9c6894da3309271d2d2abe1e2b68
Administrator <kristofer.souza2027@hotmail.com>
2023-06-11 11:32:48 -0800

    Testando kivy

---
+++ main2.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+GUI = Builder.load_file("tela.kv")
+
+
+class MeuAplicativo(App):
+    def build(self):
+        return GUI
+
+    def atualizar_texto(self, button):
+        button.text = "Deu certo"
+
+
+if __name__ == '__main__':
+    MeuAplicativo().run()
