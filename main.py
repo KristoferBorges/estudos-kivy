46bb3d7b4da1650bc5dce1c0cef657322aaca9c8
Administrator <kristofer.souza2027@hotmail.com>
2023-06-16 09:58:24 -0800

    Treinando com o kivy

--- main.py
+++ main.py
@@ -1,42 +1,15 @@
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.textinput import TextInput
-from kivy.lang import Builder
-
-GUI = Builder.load_file("tela.kv")
+from kivy.uix.gridlayout import GridLayout
 
 
-class TelaInicial(Screen):
-    numero_tela_inicial = ''
-
-    def armazenar_numero(self):
-        self.numero_tela_inicial = self.ids.input_tela1.text
+class MyGridLayout(GridLayout):
+    pass
 
 
-class TelaSecundaria(Screen):
-    numero_tela_secundaria = ''
-
-    def armazenar_numero(self):
-        self.numero_tela_secundaria = self.ids.input_tela2.text
-
-
-class TelaFinal(Screen):
-    def on_pre_enter(self):
-        numero_tela_inicial = self.manager.get_screen('tela_inicial').numero_tela_inicial
-        numero_tela_secundaria = self.manager.get_screen('tela_secundaria').numero_tela_secundaria
-        self.ids.label_tela_inicial.text = "Número da Tela Inicial: " + numero_tela_inicial
-        self.ids.label_tela_secundaria.text = "Número da Tela Secundária: " + numero_tela_secundaria
-
-
-class MeuAplicativo(App):
+class MyApp(App):
     def build(self):
-        screen_manager = ScreenManager()
-        screen_manager.add_widget(TelaInicial(name='tela_inicial'))
-        screen_manager.add_widget(TelaSecundaria(name='tela_secundaria'))
-        screen_manager.add_widget(TelaFinal(name='tela_final'))
-        return screen_manager
+        return MyGridLayout()
 
 
 if __name__ == '__main__':
-    MeuAplicativo().run()
+    MyApp().run()
